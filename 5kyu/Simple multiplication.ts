228f803593bf6723229d677a954379dc157a166f
Pantaiz <pantaiz@yandex.ru>
2023-01-15 02:38:40 +0300

    ts

---
+++ 5kyu/Simple multiplication.ts
@@ -0,0 +1,5 @@
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+export function simpleMultiplication(num: number): number{
+    return num % 2 == 0 ? num * 8 : num * 9;
+  }
