d8d5ccdfba6d48db64041de12f5f0263f0be80d1
Pantaiz_Lenovo <pantaiz@yandex.ru>
2023-01-17 00:10:02 +0300

    codeeeeeeeeeee

---
+++ 5kyu/Even or Odd.ts
@@ -0,0 +1,7 @@
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd number
+
+
+
+export function even_or_odd(n:number):string {
+return n%2==0? "Even" : "Odd";
+}
