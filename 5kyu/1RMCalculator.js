0b22b9a43f980bba37fa6b28749619dbe4d84344
Pantaiz <pantaiz@yandex.ru>
2023-01-26 22:26:49 +0300

    6kyu 1rows

--- 5kyu/1RMCalculator.js
+++ 5kyu/1RMCalculator.js
@@ -1,11 +1,1 @@
-function calculate1RM(w, r) {
-    let rm=[];
-    if (r<=1){
-        if (r==0){
-            return 0
-        }
-        return w
-    }
-    rm.push(Math.round(w*(1+r/30)),Math.round((100*w)/(101.3-2.67123*r)),Math.round(w*r**0.1))
-    return  rm.sort((a,b)=>a-b).pop()
-}
+const calculate1RM=(w, r) => r<=1?w*r:Math.round(Math.max(w*(1+r/30),(100*w)/(101.3-2.67123*r),(w*r**0.1)));
