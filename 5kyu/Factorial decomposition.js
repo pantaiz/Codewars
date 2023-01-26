cc399af149a8fb4c40f708b7d43a72795623d706
Pantaiz <pantaiz@yandex.ru>
2023-01-27 02:54:08 +0300

    5kyu hard to me

---
+++ 5kyu/Factorial decomposition.js
@@ -0,0 +1,41 @@
+function decomp(a) {
+    const factors = []
+    for (; a > 1; a--) {
+        n = a
+        ;
+        let divisor = 2;
+
+        while (n >= 2) {
+
+            if (n % divisor == 0) {
+                factors.push(divisor);
+                n = n / divisor;
+            } else {
+                divisor++;
+            }
+        }
+    }
+    //Получили массив с простыми множителями
+    let counter = 1;
+    let outputString = "";
+    factors.sort((a, b) => a - b)
+    for (let i = 1; i < factors.length; i++) {
+        if (factors[i] === factors[i - 1]) {
+            counter++
+            if (factors[i] != factors[i + 1]) {
+                outputString += factors[i] + '^' + counter + ' * '
+                counter = 0;
+            }
+        } else {
+            if (factors[i] == factors[i + 1]) {
+                counter++
+            } else {
+                outputString += factors[i] + " * "
+            }
+
+        }
+
+    }
+    return outputString.slice(0, -3);
+
+}
