0fee9357e6bf0edac45121f7204c14e078b177f7
Pantaiz <pantaiz@yandex.ru>
2023-02-11 01:44:13 +0300

    5

--- 5kyu/Land perimeter.js
+++ 5kyu/Land perimeter.js
@@ -1,31 +1,30 @@
 function landPerimeter (arr) {
-
+    let sum=0
     for (let i = 0; i <arr.length ; i++) {
-        for (let j = 0; j < arr.length; j++) {
+        for (let j = 0; j < arr[i].length; j++) {
             if (arr[i][j]==='X'){
-                let sum=1
+                sum+=4
                 if (j-1>=0 && arr[i][j-1]==="X"){
-                    sum++
+                    sum--
                 }
-                if (j+1>=arr.length && arr[i][j+1]==="X"){
-                    sum++
+                if (j+1<arr[i].length && arr[i][j+1]==="X"){
+                    sum--
                 }
                 if (i-1>=0 && arr[i-1][j]==="X"){
-                    sum++
+                    sum--
                 }
-                if (i+1>=arr.length && arr[i+1][j]==="X"){
-                    sum++
+                if (i+1<arr.length && arr[i+1][j]==="X"){
+                    sum--
                 }
-
-                console.log(arr[i][j])
             }
 
         }
     }
-    return sum
+    return`Total land perimeter: ${sum}`
 }
-console.log(landPerimeter(['XOOXO',
-    'XOOXO',
-    'OOOXO',
-    'XXOXO',
-    'OXOOO'] ))
+console.log(landPerimeter([
+    "XXX",
+    "OOX",
+    "OOO",
+    "XXX",
+    "OXO"] ))
