d58767f45b542b7dac5fb37eac38f87df4a753ad
Pantaiz <pantaiz@yandex.ru>
2023-02-20 22:41:57 +0300

    6kyu

---
+++ 5kyu/Format words into a sentence.js
@@ -0,0 +1,12 @@
+/*
+['ninja', 'samurai', 'ronin'] --> "ninja, samurai and ronin"
+['ninja', '', 'ronin'] --> "ninja and ronin"
+[] -->""
+ */
+function formatWords(words){
+    if (!words) return ''
+    words=words.filter(v=>v.length>0)
+    if (words.length===0) return ''
+    if (words.length===1) return words[0]
+    return words.slice(0,-1).join(', ')+' and '+words.slice(-1)
+}
