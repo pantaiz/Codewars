f6819c0ab986541559b4959ca5f4c187ae6ed063
Pantaiz <pantaiz@yandex.ru>
2023-03-24 00:05:02 +0300

    6kyu

--- 5kyu/Divide and Conquer.js
+++ 5kyu/Divide and Conquer.js
@@ -1,14 +1,1 @@
-function divCon(x){
-    let a=0
-    for (let i = 0; i < x.length; i++) {
-        if (typeof x[i]=='number'){
-            a+=x[i]
-        }else(
-            a+=-x[i]
-        )
-
-    }
-    return a
-}
-
-console.log(divCon([9, 3,3, '7', '3']))
+const divCon=(x)=>x.reduce((acc,a)=>typeof a==="number"?acc+a:acc-Number(a),0)
