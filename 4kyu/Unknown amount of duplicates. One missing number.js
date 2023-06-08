00edf93caeba66efdf62759a42374c6e323fd9be
Pantaiz_Lenovo <pantaiz@yandex.ru>
2023-06-08 23:29:34 +0300

    6

---
+++ 4kyu/Unknown amount of duplicates. One missing number.js
@@ -0,0 +1,10 @@
+const findNumber = (arr) => {
+    arr.sort((a, b) => a - b)
+    let double = []
+    for (let i = 0; i < arr.length; i++) {
+        
+    }
+    return arr
+}
+const arr = [10, 9, 8, 9, 6, 1, 2, 4, 3, 2, 5, 5, 3]
+console.log(findNumber(arr))
